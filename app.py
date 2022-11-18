b3c2ae5586ab0d8a775faf0254f3a2b9a39d8200
Hershade <germanxd717@gmail.com>
2022-11-18 15:47:31 -0600

    improve the structure of the file app

--- app.py
+++ app.py
@@ -1,28 +1,24 @@
 from flask import Flask
 from flask_migrate import Migrate
 
-
-from database import  db
-from models import Movie
+from database import db
+from movie.api import show_movies
 
 app = Flask(__name__)
 
-
-#configuration our db
+# configuration our db
 USER_DB = 'postgres'
 PASS_DB = 'admin'
 URL_DB = 'localhost'
 NAME_DB = 'cinema_guatemala'
 FULL_URL_DB = f'postgresql://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_DB}'
 
-#configuration our app
+# configuration our app
 app.config['SQLALCHEMY_DATABASE_URI'] = FULL_URL_DB
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
-
 # inicialization of the object db from sqlachemy
 db.init_app(app)
-
 
 # configuration flask-migrate
 migrate = Migrate()
@@ -30,6 +26,5 @@ migrate = Migrate()
 # we initialize the object to be able to execute the migrations to the database
 migrate.init_app(app, db)
 
-@app.route('/movies')
-def show_movies():
-    return 'hello world'
+
+app.add_url_rule('/movies', 'movies', show_movies, methods=['POST', 'GET'])
