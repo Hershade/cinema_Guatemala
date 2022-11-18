8812b5fa3a42c7ccc99f1d91c358b02ba8da8681
Hershade <germanxd717@gmail.com>
2022-11-17 21:13:22 -0600

    Configuration the conection to the database

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_migrate import Migrate
+
+
+from database import  db
+from models import Movie
+
+app = Flask(__name__)
+
+
+#configuration our db
+USER_DB = 'postgres'
+PASS_DB = 'admin'
+URL_DB = 'localhost'
+NAME_DB = 'cinema_guatemala'
+FULL_URL_DB = f'postgresql://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_DB}'
+
+#configuration our app
+app.config['SQLALCHEMY_DATABASE_URI'] = FULL_URL_DB
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+
+# inicialization of the object db from sqlachemy
+db.init_app(app)
+
+
+# configuration flask-migrate
+migrate = Migrate()
+
+# we initialize the object to be able to execute the migrations to the database
+migrate.init_app(app, db)
+
+@app.route('/movies')
+def show_movies():
+    return 'hello world'
