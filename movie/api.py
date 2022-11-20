1764fb979b69ae884b30571f6c13d06a943ba669
Hershade <germanxd717@gmail.com>
2022-11-19 23:51:45 -0600

    Create a method to serializer data

--- movie/api.py
+++ movie/api.py
@@ -1,10 +1,28 @@
-from flask import request, app
+from flask import request
 
 from database import db
-from models.models import Movie
+from models.models import Movie, Feature
+
+
+# Function to convert to Json
+def features_view(model, id):
+    all_features = model.query.filter_by(movies_id=id).all()
+    results = [
+            {
+                "id": feature.id,
+                "date_time": feature.date_time,
+                "created_at": feature.created_at,
+                "movies_id": feature.movies_id,
+                "rooms": feature.rooms,
+
+            }
+            for feature in all_features]
+    return results
 
 
 # @app.route('/movies', methods=['POST', 'GET'])
+
+
 def show_movies():
     if request.method == 'POST':
         if request.is_json:
@@ -18,13 +36,15 @@ def show_movies():
             return {"error": "The request payload is not in JSON format"}
     elif request.method == 'GET':
         movies = Movie.query.all()
+
         results = [
             {
                 "id": movie.id,
                 "title": movie.title,
                 "url_image": movie.url_image,
                 "classification": movie.classification,
-                "created_at": movie.created_at
+                "created_at": movie.created_at,
+                "features": features_view(Feature, movie.id)
 
             }
             for movie in movies]
